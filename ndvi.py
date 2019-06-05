3034f39e5479c5b647ba398023f7af7410844b21
JoeSvr95 <joesaverio@hotmail.com>
2019-06-05 03:14:04 -0500

    Cambio en la UI para que sea responisve

--- ndvi.py
+++ ndvi.py
@@ -7,26 +7,50 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QImage, QPixmap
-
-import numpy as np
-import cv2
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1106, 691)
+        MainWindow.resize(1090, 828)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.loadImgBtn = QtWidgets.QPushButton(self.centralwidget)
-        self.loadImgBtn.setGeometry(QtCore.QRect(10, 10, 111, 41))
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.main_frame = QtWidgets.QFrame(self.centralwidget)
+        self.main_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.main_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.main_frame.setObjectName("main_frame")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.main_frame)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tools_frame = QtWidgets.QFrame(self.main_frame)
+        self.tools_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.tools_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.tools_frame.setObjectName("tools_frame")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.tools_frame)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.loadImgBtn = QtWidgets.QPushButton(self.tools_frame)
+        self.loadImgBtn.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.loadImgBtn.sizePolicy().hasHeightForWidth())
+        self.loadImgBtn.setSizePolicy(sizePolicy)
         self.loadImgBtn.setObjectName("loadImgBtn")
-        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
-        self.scrollArea.setGeometry(QtCore.QRect(10, 60, 1081, 211))
+        self.horizontalLayout_3.addWidget(self.loadImgBtn)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.verticalLayout.addWidget(self.tools_frame)
+        self.img_frame = QtWidgets.QFrame(self.main_frame)
+        self.img_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.img_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.img_frame.setObjectName("img_frame")
+        self.gridLayout = QtWidgets.QGridLayout(self.img_frame)
+        self.gridLayout.setObjectName("gridLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.img_frame)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 1079, 209))
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 1030, 678))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.scrollAreaWidgetContents)
         self.horizontalLayout.setObjectName("horizontalLayout")
@@ -36,12 +60,12 @@ class Ui_MainWindow(object):
         self.imageLbl.setObjectName("imageLbl")
         self.horizontalLayout.addWidget(self.imageLbl)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(10, 310, 1081, 201))
-        self.graphicsView.setObjectName("graphicsView")
+        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.img_frame)
+        self.gridLayout_2.addWidget(self.main_frame, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1106, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1090, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -51,31 +75,8 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        # Connect button to method
-        self.loadImgBtn.clicked.connect(self.setImage)
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "NDVI Labeling"))
         self.loadImgBtn.setText(_translate("MainWindow", "Cargar Imagen"))
 
-    # Method to load image
-    def setImage(self):
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Cargar Imagen", "", "Image Files (*.png *.jpg *.jpeg *.bmp)")
-        if fileName:
-            image = cv2.imread(fileName)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            height, width, channel = image.shape
-            step = channel * width
-            # create QImage from image
-            qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
-            self.imageLbl.setPixmap(QPixmap.fromImage(qImg))
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
---
+++ ndvichecking.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+import cv2
+import ndvi
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QImage, QPixmap
+
+class MainNDVI(ndvi.Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainNDVI, self).__init__()
+        loadUi('ndvi.ui', self)
+        self.loadImgBtn.clicked.connect(self.setImage)
+
+    # Method to load image
+    def setImage(self):
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Cargar Imagen", "", "Image Files (*.png *.jpg *.jpeg *.bmp)")
+        if fileName:
+            image = cv2.imread(fileName)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            height, width, channel = image.shape
+            step = channel * width
+            # create QImage from image
+            qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+            self.imageLbl.setPixmap(QPixmap.fromImage(qImg))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainNDVI()
+    widget.show()
+    sys.exit(app.exec_())
