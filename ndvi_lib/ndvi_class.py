4c9a2a59afe4e1ce3e2844b5e94e56bb2525a9e7
JoeSvr95 <joesaverio@hotmail.com>
2019-06-14 03:23:17 -0500

    Cambio de la UI y mejora de las clases ImageViewer

--- ndvi_lib/ndvi_class.py
+++ ndvi_lib/ndvi_class.py
@@ -2,14 +2,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QPoint, Qt, QLineF
 from PyQt5.QtGui import QPainterPath, QPen
 
-class ImageViewer(QtWidgets.QGraphicsView):
-    start = None
-    end = None
-    item = None
-    path = None
-
+class RGBViewer(QtWidgets.QGraphicsView):
     def __init__(self, parent):
-        super(ImageViewer, self).__init__(parent)
+        super(RGBViewer, self).__init__(parent)
         # Instance variables for basic image control
         self.zoom = 0
         self.empty = True
@@ -17,12 +12,6 @@ class ImageViewer(QtWidgets.QGraphicsView):
         self._scene = QtWidgets.QGraphicsScene(self)
         self._scene.addItem(self.image)
         self.setScene(self._scene)
-        # Instance variables for selection
-        self.path = QPainterPath()
-        self.item = GraphicPathItem()
-        self.scene().addItem(self.item)
-        self.select = False
-        self.drawing = False
 
     def fitInView(self):
         rect = QtCore.QRectF(self.image.pixmap().rect())
@@ -66,8 +55,21 @@ class ImageViewer(QtWidgets.QGraphicsView):
                 self.fitInView()
             else:
                 self.zoom = 0
-    
-    # Crea el objeto painter e inicia bandera para seleccionar
+
+class NDVIViewer(RGBViewer):
+    start = None
+    end = None
+    item = None
+    path = None
+
+    def __init__(self, parent):
+        super(NDVIViewer, self).__init__(parent)
+        self.path = QPainterPath()
+        self.item = GraphicPathItem()
+        self.scene().addItem(self.item)
+        self.select = False
+        self.drawing = False
+
     def startSelectROI(self):
         self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
         self.setCursor(Qt.CrossCursor)
@@ -85,10 +87,11 @@ class ImageViewer(QtWidgets.QGraphicsView):
             self.start = self.end
             self.item.setPath(self.path)
 
+
 class GraphicPathItem(QtWidgets.QGraphicsPathItem):
     def __init__(self):
         super(GraphicPathItem, self).__init__()
         pen = QPen()
         pen.setColor(Qt.red)
-        pen.setWidth(3)
+        pen.setWidth(5)
         self.setPen(pen)
--- ndvichecking.py
+++ ndvichecking.py
@@ -4,7 +4,7 @@ import cv2
 
 from views.ndvi_ui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import pyqtSlot, QPoint, Qt
+from PyQt5.QtCore import pyqtSlot, QPoint, Qt, QFileInfo
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QFileDialog, QStatusBar
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QImage, QPixmap, QPainter, QPen
@@ -13,12 +13,6 @@ class MainNDVI(Ui_MainWindow, QMainWindow):
     def __init__(self): # Inicializa todos los atributos y los widgets
         super(MainNDVI, self).__init__()
         self.setupUi(self)
-        self.lastPoint = QPoint()
-        self.firstPoint = QPoint()
-        self.select = False
-        self.drawing = False
-        self.statusbar = QStatusBar()
-        self.setStatusBar(self.statusBar)
         self.loadRGBBtn.clicked.connect(self.loadRGBImage)
         self.loadNDVIBtn.clicked.connect(self.loadNDVIImage)
         self.selectBtn.clicked.connect(self.selectROI)
@@ -29,23 +23,25 @@ class MainNDVI(Ui_MainWindow, QMainWindow):
         fileName, _ = QFileDialog.getOpenFileName(None, "Cargar Imagen", "", "Image Files (*.png *.jpg *.jpeg *.bmp *.tiff)")
         if fileName:
             pixmap = QPixmap(fileName)
+            file_info = QFileInfo(fileName)
             #pixmap = pixmap.scaled(self.ndvi_view.width(), self.ndvi_view.height(), QtCore.Qt.KeepAspectRatio)
             size = pixmap.size()
             self.rgb_view.setImage(pixmap)
-            self.statusBar.showMessage("Resolución: " + str(size.width()) + "x" + str(size.height()))
+            self.rgb_info.setText("Resolución: " + str(size.width()) + "x" + str(size.height()) + ", Tamaño: " + str(file_info.size()))
     
     def loadNDVIImage(self):
         fileName, _ = QFileDialog.getOpenFileName(None, "Cargar Imagen", "", "Image Files (*.png *.jpg *.jpeg *.bmp *.tiff)")
         if fileName:
             pixmap = QPixmap(fileName)
+            file_info = QFileInfo(fileName)
             #pixmap = pixmap.scaled(self.ndvi_view.width(), self.ndvi_view.height(), QtCore.Qt.KeepAspectRatio)
             size = pixmap.size()
             self.ndvi_view.setImage(pixmap)
-            self.statusBar.showMessage("Resolución: " + str(size.width()) + "x" + str(size.height()))
+            self.ndvi_info.setText("Resolución: " + str(size.width()) + "x" + str(size.height()) + ", Tamaño: " + str(file_info.size()))
 
     # Método para habilitar la opción de selección
     def selectROI(self):
-        self.rgb_view.startSelectROI()
+        self.ndvi_view.startSelectROI()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
