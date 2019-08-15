a7b1605cec79bf115a160ba477e2f21e4f60e834
JoeSvr95 <joesaverio@hotmail.com>
2019-08-15 11:46:33 -0500

    Cambios en el script de regresión lineal

--- SimpleLinearRegressionSpreadsheet.py
+++ SimpleLinearRegressionSpreadsheet.py
@@ -1,7 +1,8 @@
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, metrics
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from openpyxl import load_workbook
 
@@ -35,44 +36,62 @@ sheets = doc.sheetnames
 hoja = doc[sheets[0]]
 filas = hoja.max_row+1
 
-# Obtener datos de x - Valor NDVIO, y - Valor Clorofila Espectrometro
+# Obtener datos de x - Valor NDVIO, y - Valor Clorofila SPAD
 x = []
-yspec = []
+yspad = []
 for i in range(3,filas):
-    spec = float(hoja[i][0].value)
+    spec = float(hoja[i][12].value)
     ndvi = float(hoja[i][10].value)
     if ndvi > 0.1:
         x.append([ndvi])
-        yspec.append(spec)
+        yspad.append(spec)
 
-################ CLOROFILA LABORATORIO ################
+################ CLOROFILA SPAD ################
 print("\nNDVI:\n", x)
-print("\nSPEC:\n", yspec)
+print("\nSPEC:\n", yspad)
+print("\nNDVI:\n", len(x))
+print("\nSPEC:\n", len(yspad))
 
 # Gráfica de los datos
-plt.scatter(x, yspec)
-plt.title('Clorofila Espectrometro')
+plt.scatter(x, yspad)
+plt.title('Clorofila SPAD')
 plt.xlabel('Valor NDVI')
-plt.ylabel('Valor Clorofila Espectrometro')
+plt.ylabel('Valor Clorofila SPAD')
 plt.show()
 
 # Entrena con el 75% y predice con el 25% restante
-lrs, xTrain, yTrain, xTest, yTest, yPred = train(x, yspec, 1, 0.2)
+lrs, xTrain, yTrain, xTest, yTest, yPred = train(x, yspad, 1, 1)
 
-print('\nPREDICCIÓN CLOROFILA ESPECTROMETRO')
+print('\nPREDICCIÓN CLOROFILA SPAD')
 print('\nDatos de NDVI:\n', xTest)
-print('\nDatos de Valor de Clorofila Espectrometro reales:\n', yTest)
-print('\nDatos de Valor de Clorofila Espectrometro predichos:\n', yPred)
+print('\nDatos de Valor de Clorofila SPAD reales:\n', yTest)
+print('\nDatos de Valor de Clorofila SPAD predichos:\n', yPred)
 
 # Gráfica de los datos con la regresión lineal simple
 plt.scatter(xTest, yTest)
 plt.plot(xTest, yPred, color='red')
-plt.title('Clorofila Espectrometro - Regresión Lineal Simple')
+plt.title('Clorofila SPAD - Regresión Lineal Simple')
 plt.xlabel('Valor NDVI')
-plt.ylabel('Valor Clorofila Espectrometro')
+plt.ylabel('Valor Clorofila SPAD')
 plt.show()
 
 print('\nPendiente (a): ', lrs.coef_)
 print('Intersección (b): ', lrs.intercept_)
 print('Ecuación (y = ax + b): ', 'y = ', lrs.coef_, 'x ', lrs.intercept_)
 print('Precisión del modelo: ', lrs.score(xTrain, yTrain))
+
+# Valores predichos vs manuales
+lrs1 = linear_model.LinearRegression()
+yMan = []
+for i in yTest:
+    yMan.append([i])
+
+lrs1.fit(yMan, yPred)
+print('\nRaíz del error cuadrático medio: ', np.sqrt(metrics.mean_squared_error(yTest, yPred)))
+print('\nPendiente (a): ', lrs1.coef_)
+print('Intersección (b): ', lrs1.intercept_)
+print('Ecuación (y = ax + b): ', 'y = ', lrs1.coef_, 'x ', lrs1.intercept_)
+print('Correlación lineal entre predichos y manuales: ', lrs1.score(yMan, yPred))
+
+sns.distplot((yTest - yPred), bins = 50)
+plt.show()
